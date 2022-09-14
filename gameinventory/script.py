f682e16df73704301b7858e783fbb57aa32555d1
James Driscoll <thejamesdriscoll@gmail.com>
2022-09-14 15:24:04 +0200

    converted to f strings

--- gameinventory/script.py
+++ gameinventory/script.py
@@ -3,9 +3,9 @@ inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 def displayInventory(inv):
     total_items = 0
     for k,v in inv.items():
-        print(k+": " + str(v))
+        print(f"{k}: {v}")
         total_items += 1
-    print("Total number of items: " + str(total_items))
+    print(f"Total number of items: {total_items}")
 
 def addToInventory(inv,addedItems):
     for item in addedItems:
