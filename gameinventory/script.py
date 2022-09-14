a09f7f2c9795e67ca734352d3e7689ce8f477c2e
James Driscoll <thejamesdriscoll@gmail.com>
2022-09-14 15:03:06 +0200

    wrote function to display total number of items in inventory

---
+++ gameinventory/script.py
@@ -0,0 +1,9 @@
+inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def displayInventory(inv):
+    total_items = 0
+    for k,v in inv.items():
+        print(k+": " + str(v))
+        total_items += v
+    print("Total number of items: " + str(total_items))
+displayInventory(inv)
